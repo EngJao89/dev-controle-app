04ee68714d8a5c8fb65cdf4f7f1e31d1395a6b33
EngJao89 <eng_joao@outlook.com>
2024-07-11 15:58:49 -0300

    feat(dca06-user-session-redirect): implementa verificação de sessão de usuário

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,16 @@
 import { Container } from '@/components/Container'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
+import { redirect } from 'next/navigation'
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions)
+
+  if (!session || !session.user) {
+    redirect("/")
+  }
+
+
   return (
     <Container>
       <h1>Página dashboard</h1>
